4c9f4024018bebec3f1667c00d404cc3613f0e61
adolfojb2 <adolfojb2@gmail.com>
2025-03-23 09:49:15 -0500

    Actualización de los proyectos

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all() #Trae todos los proyectos
+    return render(request, 'home.html',{'projects':projects})
